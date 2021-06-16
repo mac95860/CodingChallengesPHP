1e2c610c51ccc749b68d0ab50474dadb0732c74b
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-06-16 10:38:09 -0500

    find the discount

---
+++ find-the-discount.php
@@ -0,0 +1,15 @@
+<!-- Create a function that takes two arguments: 
+the original price and the discount percentage as integers 
+and returns the final price after the discount. -->
+
+<?php
+    function getDiscount($total, $discount) {
+        $amountOff = $discount / 100;
+        $newTotal = $total * $amountOff;
+        echo $newTotal . "<br>";
+    };
+
+    getDiscount(1500, 50); // 750
+    getDiscount(89, 20); // 71.2
+    getDiscount(100, 75); // 25
+?>
