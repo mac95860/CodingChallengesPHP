e222dc8d708bbe60898905c2cdd4139deeb33e72
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-07-03 17:50:11 -0500

    contains prime

---
+++ contains-prime.php
@@ -0,0 +1,29 @@
+<!-- Create a function that returns true if every number is prime and false if not -->
+
+<?php
+    function containsPrime($arr) {
+        $checkForPrime = array_map(function($num) {
+            if($num === 1) {
+                return 0;
+            }
+
+            for($i = 2; $i < $num; $i++) {
+                if($num % $i == 0) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        }, $arr);
+
+        if(in_array(0, $checkForPrime, TRUE)) {
+            echo false;
+        } else {
+            echo true;
+        };
+    }
+    
+    containsPrime([5,2,1,5,6,7]); //false
+    containsPrime([2, 3, 5, 7, 13, 17, 19, 23, 29]);// true
+    containsPrime([1, 5, 3]); // false
+?>
