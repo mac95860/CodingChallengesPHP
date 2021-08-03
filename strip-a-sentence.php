96588fc3a12f4ec6c1e6580af8ed0ccb0def6a6d
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-08-03 13:51:37 -0500

    oops

--- strip-a-sentence.php
+++ strip-a-sentence.php
@@ -11,5 +11,5 @@
 
    echo stripSentence("the quick brown fox jumps over the lazy dog", "aeiou"); //th qck brwn fx jmps vr th lzy dg
    echo stripSentence("the hissing snakes sinisterly slither across the rustling leaves", "s"); // "the hiing nake initerly lither acro the rutling leave"
-   stripSentence("gone, reduced to atoms", "go, muscat nerd") ; // ""
+   echo stripSentence("gone, reduced to atoms", "go, muscat nerd") ; // ""
 ?>
