5fa3d26f8b6d519823b4c599039edb75cb407bf7
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-11-21 10:26:15 -0600

    absolute sum

---
+++ absolute-sum.php
@@ -0,0 +1,17 @@
+<?php
+
+// Create a function that return the sum of all the absolute values in an array
+
+	function getAbsSum($arr) {
+        $sum = 0;
+        foreach($arr as $value) {
+            $sum += abs($value);
+        }
+        return $sum;
+    }
+
+    echo(
+        getAbsSum([2,5,-7])
+    );
+	
+?>
